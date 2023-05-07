571f91ff39c4048a3c65005895f0031a1abd6c33
Raul <fsdfaaul@gmail.com>
2023-05-07 13:19:09 +0500

    add all pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
-urlpatterns =[
-    path('', views.home_view, name='home')
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('pricing/', views.pricing_view, name='pricing'),
+
+    path('about/', views.about_html, name='about'),
+
+    path('products/', views.products_html, name='products'),
+
+    path('faq/', views.faq_html, name='faq'),
+
+    path('contact/', views.contact_html, name='contact')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
+
 def home_view(request):
-    return HttpResponse('Hello world')
+    return render(request, 'pages/index.html')
+
+
+def pricing_view(request):
+    return render(request, 'pages/pricing.html')
+
+
+def about_html(request):
+    return render(request, 'pages/about.html')
+
+
+def products_html(request):
+    return render(request, 'pages/products.html')
+
+
+def faq_html(request):
+    return render(request, 'pages/faq.html')
+
+
+def contact_html(request):
+    return render(request, 'pages/contact.html')
